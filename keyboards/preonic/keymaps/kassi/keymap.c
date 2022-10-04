9d3fd72c035de0cc8faba5e423c5b25fedd8508f
kassi <caffeinated.kassi@gmail.com>
2022-10-04 15:08:56 -0500

    Preonic changes

--- keyboards/preonic/keymaps/kassi/keymap.c
+++ keyboards/preonic/keymaps/kassi/keymap.c
@@ -19,7 +19,10 @@
 
 enum preonic_layers {
   _QWERTY,
-  _NAV
+  _SYMBOL,
+  _GAME,
+  _NAV,
+  _LAYER_SWITCH
 };
 
 // enum preonic_keycodes {
@@ -40,11 +43,11 @@ enum preonic_layers {
  * '-----------------------------------------------------------------------------------'
  */
 // [_NAV] = LAYOUT_preonic_grid(
-//   KC_NO,   KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,      KC_NO,   KC_NO,   KC_NO,   KC_NO,
-//   KC_NO,   KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,      KC_NO,   KC_NO,   KC_NO,   KC_NO,
-//   KC_NO,   KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,      KC_NO,   KC_NO,   KC_NO,   KC_NO,
-//   KC_NO,   KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,      KC_NO,   KC_NO,   KC_NO,   KC_NO,
-//   KC_NO,   KC_NO,    KC_NO,    KC_NO,      KC_NO,_______,         KC_NO,_______,   KC_NO,   KC_NO,   KC_NO,   KC_NO
+//   KC_TRNS,   KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,      KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,
+//   KC_TRNS,   KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,      KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,
+//   KC_TRNS,   KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,      KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,
+//   KC_TRNS,   KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,      KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,
+//   KC_TRNS,   KC_TRNS,    KC_TRNS,    KC_TRNS,    _______,KC_TRNS,        _______,KC_TRNS,          KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS
 // )
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -59,15 +62,57 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |Shift |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |ENT(SHFT)|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | ESC  | GUI  |      |Lower |  Bksp(ALT)  |    Space    |  [   |  ]   |      | GAME |
+ * | ESC  | GUI  |      |Lower |  Bksp(ALT)  |    Space    |  [   |  ]   |      | Layer Switch |
  * '-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_preonic_grid(
-  KC_EQUAL, KC_1,    KC_2,    KC_3,    KC_4,           KC_5,         KC_6,    KC_7,    KC_8,        KC_9,        KC_0,    KC_MINS,
-  KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,           KC_T,         KC_Y,    KC_U,    KC_I,        KC_O,        KC_P,    KC_BSLS,
-  KC_LCTL,  KC_A,    KC_S,    KC_D,    LT(_NAV, KC_F), KC_G,         KC_H,    KC_J,    KC_K,        KC_L,        KC_SCLN, KC_QUOT,
-  KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,           KC_B,         KC_N,    KC_M,    KC_COMM,     KC_DOT,      KC_SLSH, MT(MOD_RSFT, KC_ENT),
-  KC_ESC,   KC_LGUI, KC_NO,   KC_NO,   KC_NO,MT(MOD_LALT,KC_BSPC),   KC_NO,KC_SPC,    KC_LBRACKET, KC_RBRACKET, KC_NO,   KC_NO
+  KC_EQUAL, KC_1,    KC_2,    KC_3,          KC_4,           KC_5,         KC_6,    KC_7,    KC_8,        KC_9,        KC_0,    KC_MINS,
+  KC_TAB,   KC_Q,    KC_W,    KC_E,          KC_R,           KC_T,         KC_Y,    KC_U,    KC_I,        KC_O,        KC_P,    KC_BSLS,
+  KC_LCTL,  KC_A,    KC_S,    KC_D,          LT(_NAV, KC_F), KC_G,         KC_H,    KC_J,    KC_K,        KC_L,        KC_SCLN, KC_QUOT,
+  KC_LSFT,  KC_Z,    KC_X,    KC_C,          KC_V,           KC_B,         KC_N,    KC_M,    KC_COMM,     KC_DOT,      KC_SLSH, MT(MOD_RSFT, KC_ENT),
+  KC_ESC,   KC_LGUI, KC_NO,   MO(_SYMBOL),  _______,LALT_T(KC_BSPC),      _______,KC_SPC,    KC_LBRACKET, KC_RBRACKET, KC_NO,   MO(_LAYER_SWITCH)
+),
+
+/* Symbol
+ * .-----------------------------------------------------------------------------------.
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |             |             |      |      |      |      |
+ * '-----------------------------------------------------------------------------------'
+ */
+[_SYMBOL] = LAYOUT_preonic_grid(
+  KC_TRNS,   KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,      KC_F7,           KC_F8,     KC_F9,     KC_F10,    KC_F11,
+  KC_TRNS,   KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,         KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_F12,
+  KC_TRNS,   KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    LSFT(KC_GRAVE),  KC_GRV,    KC_LBRC,   KC_RBRC,   KC_TRNS,
+  KC_TRNS,   KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,         KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,
+  KC_TRNS,   KC_TRNS,    KC_TRNS,    KC_TRNS,    _______,KC_TRNS,        _______,KC_TRNS,             KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS
+),
+
+/* Game
+ * .-----------------------------------------------------------------------------------.
+ * | ESC  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | BSPC |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Tab  |   Q  |   W  |   E  |   R  |   T  |      |      |      |      |      |      |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * | Ctrl |   A  |   S  |   D  |   F  |   G  |      |      |      |  UP  |      | ENT  |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * |Shift |   Z  |   X  |   C  |   V  |   B  |      |      | LEFT | DOWN |RIGHT |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | ESC  | GUI  |      | Alt  |    Space    |    Enter    |      |      |      |QWRTY |
+ * '-----------------------------------------------------------------------------------'
+ */
+[_GAME] = LAYOUT_preonic_grid(
+  KC_ESC,   KC_1,    KC_2,    KC_3,    KC_4,           KC_5,         KC_6,    KC_7,    KC_8,      KC_9,       KC_0,      KC_BSPC,
+  KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,           KC_T,         KC_NO,   KC_NO,   KC_NO,     KC_NO,      KC_NO,     KC_NO,
+  KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,           KC_G,         KC_NO,   KC_NO,   KC_NO,     KC_UP,      KC_NO,     KC_ENT,
+  KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,           KC_B,         KC_NO,   KC_NO,   KC_LEFT,   KC_DOWN,    KC_RIGHT,  KC_NO,
+  KC_ESC,   KC_LGUI, KC_NO,   KC_LALT, _______,KC_SPC,               _______,KC_ENT,   KC_NO,     KC_NO,      KC_NO,     TO(_QWERTY)
 ),
 
 /* Navigation Layer
@@ -76,7 +121,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      | TRNS |      | Left | Down |  Up  |Right |      |      |
+ * |      |      |      | DEL  | TRNS |      | Left | Down |  Up  |Right |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -85,10 +130,31 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_NAV] = LAYOUT_preonic_grid(
   KC_TRNS,    KC_TRNS,     KC_TRNS,     KC_TRNS,     KC_TRNS,     KC_TRNS,              KC_TRNS,     KC_TRNS,     KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,
-  KC_TRNS,    KC_TRNS,     KC_TRNS,     KC_TRNS,     KC_TRNS,     KC_TRNS,              KC_TRNS,     KC_TRNS,     KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,
-  KC_TRNS,    KC_TRNS,     KC_TRNS,     KC_TRNS,     KC_TRNS,     KC_TRNS,              KC_LEFT,     KC_DOWN,     KC_UP,      KC_RIGHT,   KC_TRNS,    KC_TRNS,
+  KC_TRNS,    KC_TRNS,     KC_TRNS,     KC_TRNS,     KC_TRNS,     KC_TRNS,              KC_TRNS,     KC_HOME,     KC_END,     KC_TRNS,    KC_TRNS,    KC_TRNS,
+  KC_TRNS,    KC_TRNS,     KC_TRNS,     KC_DEL,      KC_TRNS,     KC_TRNS,              KC_LEFT,     KC_DOWN,     KC_UP,      KC_RIGHT,   KC_TRNS,    KC_TRNS,
   KC_LSFT,    KC_TRNS,     KC_TRNS,     KC_TRNS,     KC_TRNS,     KC_TRNS,              KC_TRNS,     KC_TRNS,     KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,
   KC_TRNS,    KC_TRNS,     KC_TRNS,     KC_TRNS,     _______,MT(MOD_LCTL, KC_BSPC),     _______,KC_TRNS,          KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS
+),
+
+/* Layer Switch
+ * .-----------------------------------------------------------------------------------.
+ * |      |      |      |      |      |      |      |      |      |      |      | GAME |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |             |             |      |      |      | TRNS |
+ * '-----------------------------------------------------------------------------------'
+ */
+[_LAYER_SWITCH] = LAYOUT_preonic_grid(
+  KC_TRNS,   KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,      KC_TRNS,   KC_TRNS,   KC_TRNS,   TO(_GAME),
+  KC_TRNS,   KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,      KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,
+  KC_TRNS,   KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,      KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,
+  KC_TRNS,   KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,      KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,
+  KC_TRNS,   KC_TRNS,    KC_TRNS,    KC_TRNS,    _______,KC_TRNS,        _______,KC_TRNS,          KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS
 )
 
 
@@ -139,5 +205,14 @@ bool music_mask_user(uint16_t keycode) {
     //   return false;
     default:
       return true;
+  }
+}
+
+uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case LALT_T(KC_BSPC):
+      return -1;
+    default:
+      return TAPPING_TERM;
   }
 }
